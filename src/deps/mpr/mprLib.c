432116bd9b5a30e25c92dd7beb573cf32ef5a7bc
Embedthis Software <dev@embedthis.com>
2010-06-13 19:27:51 -0700

    Update deps

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -18913,8 +18913,13 @@ static int getTimeZoneOffsetFromTm(MprCtx ctx, struct tm *tp)
 #else
     struct timezone     tz;
     struct timeval      tv;
+    int offset;
     gettimeofday(&tv, &tz);
-    return -tz.tz_minuteswest * MS_PER_MIN;
+    offset = -tz.tz_minuteswest * MS_PER_MIN;
+    if (tp->tm_isdst) {
+        offset += MS_PER_HOUR;
+    }
+    return offset;
 #endif
 }
 
@@ -19041,7 +19046,7 @@ static void decodeTime(MprCtx ctx, struct tm *tp, MprTime when, bool local)
     year = getYear(when);
 
     tp->tm_year     = year - 1900;
-#if OLD
+#if UNUSED
     tp->tm_hour     = (int) ((when / MS_PER_HOUR) % 24);
     tp->tm_min      = (int) ((when / MS_PER_MIN) % 60);
     tp->tm_sec      = (int) ((when / MS_PER_SEC) % 60);
@@ -19585,7 +19590,7 @@ char *mprFormatTime(MprCtx ctx, cchar *fmt, struct tm *tp)
             break;
 
         case 's':                                       /* seconds since epoch */
-            mprPutFmtToBuf(buf, "%d", mprMakeUniversalTime(ctx, tp) / MS_PER_SEC);
+            mprPutFmtToBuf(buf, "%d", mprMakeTime(ctx, tp) / MS_PER_SEC);
             break;
 
         case 'T':
@@ -19845,7 +19850,7 @@ int mprParseTime(MprCtx ctx, MprTime *time, cchar *dateString, int zoneFlags, st
                 *time = value;
                 mprFree(str);
                 return 0;
-            } else if (value > 32 || (tm.tm_mday >= 0 && tm.tm_year < 0)) {
+            } else if (value > 32 || (tm.tm_mday >= 0 && tm.tm_year == -MAXINT)) {
                 if (value >= 1000) {
                     fullYear = 1;
                 }
@@ -20014,7 +20019,7 @@ static void validateTime(MprCtx ctx, struct tm *tp, struct tm *defaults)
         swapDayMonth(tp);
     }
 
-    if (tp->tm_year >= 0 && tp->tm_mon >= 0 && tp->tm_mday >= 0 && tp->tm_hour >= 0) {
+    if (tp->tm_year != -MAXINT && tp->tm_mon >= 0 && tp->tm_mday >= 0 && tp->tm_hour >= 0) {
         /*  Everything defined */
         return;
     }
