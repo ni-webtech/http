67727f337fa76d7fd20437b28466012a5b8a5c13
Embedthis Software <dev@embedthis.com>
2010-08-04 12:10:10 -0700

    Refactor tracing

---
+++ src/trace.c
@@ -0,0 +1,166 @@
+/*
+    trace.c -- Trace data
+    Copyright (c) All Rights Reserved. See copyright notice at the bottom of the file.
+ */
+
+/********************************* Includes ***********************************/
+
+#include    "http.h"
+
+/*********************************** Code *************************************/
+
+void httpInitTrace(HttpTrace *trace)
+{
+    int     dir;
+
+    mprAssert(trace);
+
+    for (dir = 0; dir < HTTP_TRACE_MAX_DIR; dir++) {
+        trace[dir].levels[HTTP_TRACE_CONN] = 2;
+        trace[dir].levels[HTTP_TRACE_FIRST] = 2;
+        trace[dir].levels[HTTP_TRACE_HEADER] = 3;
+        trace[dir].levels[HTTP_TRACE_BODY] = 4;
+        trace[dir].size = -1;
+    }
+}
+
+
+/*
+    Return the level at which tracing should occur
+ */
+int httpShouldTrace(HttpConn *conn, int dir, int item, cchar *ext)
+{
+    HttpTrace   *trace;
+    int         mprLevel;
+
+    mprAssert(0 <= dir && dir < HTTP_TRACE_MAX_DIR);
+    mprAssert(0 <= item && item < HTTP_TRACE_MAX_ITEM);
+
+    trace = &conn->trace[dir];
+    if (trace->disable) {
+        return -1;
+    }
+    if (ext) {
+        if (trace->include && !mprLookupHash(trace->include, ext)) {
+            trace->disable = 1;
+            return -1;
+        }
+        if (trace->exclude && mprLookupHash(trace->exclude, ext)) {
+            trace->disable = 1;
+            return -1;
+        }
+    }
+    mprLevel = mprGetLogLevel(conn);
+    if (trace->levels[item] <= mprLevel) {
+        return trace->levels[item];
+    }
+    return -1;
+}
+
+
+static void traceBuf(HttpConn *conn, int dir, int level, cchar *msg, cchar *buf, int len)
+{
+    cchar       *cp, *tag, *digits;
+    char        *data, *dp;
+    static int  txSeq = 0;
+    static int  rxSeq = 0;
+    int         seqno, i, printable;
+
+    for (printable = 1, i = 0; i < len; i++) {
+        if (!isascii(buf[i])) {
+            printable = 0;
+        }
+    }
+    if (dir == HTTP_TRACE_TX) {
+        tag = "Transmit";
+        seqno = txSeq++;
+    } else {
+        tag = "Receive";
+        seqno = rxSeq++;
+    }
+    if (printable) {
+        data = mprAlloc(conn, len + 1);
+        memcpy(data, buf, len);
+        data[len] = '\0';
+        mprRawLog(conn, level, "\n>>>>>>>>>> %s %s packet %d, len %d (conn %d) >>>>>>>>>>\n%s", tag, msg, seqno, 
+            len, conn->seqno, data);
+        mprFree(data);
+    } else {
+        mprRawLog(conn, level, "\n>>>>>>>>>> %s %s packet %d, len %d (conn %d) >>>>>>>>>> (binary)\n", tag, msg, seqno, 
+            len, conn->seqno);
+        data = mprAlloc(conn, len * 3 + ((len / 16) + 1) + 1);
+        digits = "0123456789ABCDEF";
+        for (i = 0, cp = buf, dp = data; cp < &buf[len]; cp++) {
+            *dp++ = digits[(*cp >> 4) & 0x0f];
+            *dp++ = digits[*cp++ & 0x0f];
+            *dp++ = ' ';
+            if ((++i % 16) == 0) {
+                *dp++ = '\n';
+            }
+        }
+        *dp++ = '\n';
+        *dp = '\0';
+        mprRawLog(conn, level, "%s", data);
+    }
+    mprRawLog(conn, level, "<<<<<<<<<< %s packet, conn %d\n\n", tag, conn->seqno);
+}
+
+
+void httpTraceContent(HttpConn *conn, int dir, int item, HttpPacket *packet, int len, int total)
+{
+    HttpTrace   *trace;
+    int         size, level;
+
+    trace = &conn->trace[dir];
+    level = trace->levels[item];
+
+    if (trace->size >= 0 && total >= trace->size) {
+        mprLog(conn, level, "Abbreviating response trace for conn %d", conn->seqno);
+        trace->disable = 1;
+        return;
+    }
+    if (len <= 0) {
+        len = INT_MAX;
+    }
+    if (packet->prefix) {
+        size = mprGetBufLength(packet->prefix);
+        size = min(size, len);
+        traceBuf(conn, dir, level, "prefix", mprGetBufStart(packet->prefix), size);
+    }
+    if (packet->content) {
+        size = mprGetBufLength(packet->content);
+        size = min(size, len);
+        traceBuf(conn, dir, level, "content", mprGetBufStart(packet->content), size);
+    }
+}
+
+
+/*
+    @copy   default
+
+    Copyright (c) Embedthis Software LLC, 2003-2010. All Rights Reserved.
+    Copyright (c) Michael O'Brien, 1993-2010. All Rights Reserved.
+
+    This software is distributed under commercial and open source licenses.
+    You may use the GPL open source license described below or you may acquire
+    a commercial license from Embedthis Software. You agree to be fully bound
+    by the terms of either license. Consult the LICENSE.TXT distributed with
+    this software for full details.
+
+    This software is open source; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 2 of the License, or (at your
+    option) any later version. See the GNU General Public License for more
+    details at: http://www.embedthis.com/downloads/gplLicense.html
+
+    This program is distributed WITHOUT ANY WARRANTY; without even the
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+    This GPL license does NOT permit incorporating this software into
+    proprietary programs. If you are unable to comply with the GPL, you must
+    acquire a commercial license to use this software. Commercial licenses
+    for this software and support services are available from Embedthis
+    Software at http://www.embedthis.com
+ 
+    @end
+ */
