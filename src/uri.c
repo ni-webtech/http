5cb7caa900bfdac37b5724dfd958f6d9c6b96cad
Embedthis Software <dev@embedthis.com>
2010-09-09 00:04:11 -0700

    Fix normalize uri

--- src/uri.c
+++ src/uri.c
@@ -527,7 +527,7 @@ void httpNormalizeUri(HttpUri *uri)
 char *httpNormalizeUriPath(MprCtx ctx, cchar *pathArg)
 {
     char    *dupPath, *path, *sp, *dp, *mark, **segments;
-    int     j, i, nseg, len;
+    int     firstc, j, i, nseg, len;
 
     if (pathArg == 0 || *pathArg == '\0') {
         return mprStrdup(ctx, "");
@@ -543,6 +543,7 @@ char *httpNormalizeUriPath(MprCtx ctx, cchar *pathArg)
         return NULL;
     }
     nseg = len = 0;
+    firstc = *dupPath;
     for (mark = sp = dupPath; *sp; sp++) {
         if (*sp == '/') {
             *sp = '\0';
@@ -566,7 +567,9 @@ char *httpNormalizeUriPath(MprCtx ctx, cchar *pathArg)
                     j--;
                 }
             } else if (sp[1] == '.' && sp[2] == '\0')  {
-                if ((i+1) == nseg) {
+                if (i == 1 && *segments[0] == '\0') {
+                    j = 0;
+                } else if ((i+1) == nseg) {
                     if (--j >= 0) {
                         segments[j] = "";
                     }
@@ -580,13 +583,12 @@ char *httpNormalizeUriPath(MprCtx ctx, cchar *pathArg)
     }
     nseg = j;
     mprAssert(nseg >= 0);
-
     if ((path = mprAlloc(ctx, len + nseg + 1)) != 0) {
         for (i = 0, dp = path; i < nseg; ) {
             strcpy(dp, segments[i]);
             len = strlen(segments[i]);
             dp += len;
-            if (++i < nseg) {
+            if (++i < nseg || (nseg == 1 && *segments[0] == '\0' && firstc == '/')) {
                 *dp++ = '/';
             }
         }
