e912cf8d4064a5ec792056f5ffc8231c4db2d101
Embedthis Software <dev@embedthis.com>
2011-03-08 15:55:49 -0800

    Fix building without SSL

--- src/location.c
+++ src/location.c
@@ -115,9 +115,11 @@ static void graduate(HttpLoc *loc)
 
 void httpFinalizeLocation(HttpLoc *loc)
 {
+#if BLD_FEATURE_SSL
     if (loc->ssl) {
         mprConfigureSsl(loc->ssl);
     }
+#endif
 }
 
 
