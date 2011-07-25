2e51b10811eabe30aac7ab7de02f95600bdf3f4b
Embedthis Software <dev@embedthis.com>
2011-07-25 14:02:09 -0700

    Update MPR

--- src/deps/mpr/mprLib.c
+++ src/deps/mpr/mprLib.c
@@ -10996,6 +10996,32 @@ int mprAddItem(MprList *lp, cvoid *item)
 }
 
 
+int mprAddNullItem(MprList *lp)
+{
+    int     index;
+
+    mprAssert(lp);
+    mprAssert(lp->capacity >= 0);
+    mprAssert(lp->length >= 0);
+
+    lock(lp);
+    if (lp->length != 0 && lp->items[lp->length - 1] == 0) {
+        index = lp->length - 1;
+    } else {
+        if (lp->length >= lp->capacity) {
+            if (growList(lp, 1) < 0) {
+                unlock(lp);
+                return MPR_ERR_TOO_MANY;
+            }
+        }
+        index = lp->length;
+        lp->items[index] = 0;
+    }
+    unlock(lp);
+    return index;
+}
+
+
 /*
     Insert an item to the list at a specified position. We insert before the item at "index".
     ie. The inserted item will go into the "index" location and the other elements will be moved up.
