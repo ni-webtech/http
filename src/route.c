78eda9e0895cc57babb24c052e49217ca285a067
Embedthis Software <dev@embedthis.com>
2011-10-10 23:20:43 -0700

    Cleanup

--- src/route.c
+++ src/route.c
@@ -2221,7 +2221,9 @@ void httpAddRouteSet(HttpRoute *parent, cchar *set)
 {
     if (scasematch(set, "simple")) {
         httpAddHomeRoute(parent);
+#if UNUSED
         httpAddStaticRoute(parent);
+#endif
 
     } else if (scasematch(set, "mvc")) {
         httpAddHomeRoute(parent);
