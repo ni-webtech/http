81231532b0d3880f747911fc997bc7b95b36a05f
Embedthis Software <dev@embedthis.com>
2011-12-01 22:44:45 -0800

    Fix memory leak with pcre pattern in routes

--- src/route.c
+++ src/route.c
@@ -1359,6 +1359,9 @@ static void finalizePattern(HttpRoute *route)
     if (mprGetListLength(route->tokens) == 0) {
         route->tokens = 0;
     }
+    if (route->patternCompiled) {
+        free(route->patternCompiled);
+    }
     if ((route->patternCompiled = pcre_compile2(route->optimizedPattern, 0, 0, &errMsg, &column, NULL)) == 0) {
         mprError("Can't compile route. Error %s at column %d", errMsg, column); 
     }
